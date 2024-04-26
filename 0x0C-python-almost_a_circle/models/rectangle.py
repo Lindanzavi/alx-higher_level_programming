d234d39dad948f9887429eab7cd245c30a73f389
Lindanzavi <lindanzavi@gmail.com>
2024-04-26 14:25:12 -0700

    rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -54,12 +54,12 @@ class Rectangle(Base):
         self.validate_integer("y", value)
         self.__y = value
 
-    def valilue, eq=True):
+    def validate_integer(self, name, value, eq=True):
         '''Method for validating the value.'''
         if type(value) != int:
-raise TypeError("{} must be an integer".format(name))
+            raise TypeError("{} must be an integer".format(name))
         if eq and value < 0:
-             raise ValueError("{} must be >= 0".format(name))
+            raise ValueError("{} must be >= 0".format(name))
         elif not eq and value <= 0:
             raise ValueError("{} must be > 0".format(name))
 
