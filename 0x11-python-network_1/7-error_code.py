e00b790c049bb09cec042f8486d749fc559d2dd7
Lindanzavi <lindanzavi@gmail.com>
2024-07-09 15:22:26 -0700

    7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 """
-script that takes in a URL and an email, sends a POST request to the passed
-URL with the email as a parameter, and displays the body of the response.
+script that takes in a URL, sends a request to the URL and displays the
+body of the response (decoded in utf-8).
+If the HTTP status code is greater than or equal to 400, print:
+Error code: followed by the value of the HTTP status code.
 """
-import requests
 import sys
+import requests
 
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    email = sys.argv[2]
-    payload = {
-        "email": email
-    }
-    response = requests.post(url, data=payload)
-    print(response.text)
+    response = requests.get(url)
+    body = response.text
+    if response.status_code >= 400:
+        print("Error code: {}".format(response.status_code))
+    else:
+        print(body)
